e11203ae6f0aedb169804e0ef6f198c9ee354a9e
rohini nikam <rohinipatil0114@gmail.com>
2026-03-22 15:13:32 +0530

    updates in write room

--- AKdraft/src/pages/WriteRoom.tsx
+++ AKdraft/src/pages/WriteRoom.tsx
@@ -64,11 +64,11 @@ const WriteRoom = () => {
 
         {/* Buttons */}
         <div className="flex justify-end gap-3">
-          <button className="px-4 py-2 border rounded-md hover:bg-gray-100 transition">
+          <button className="px-7 py-4 text-2xl border rounded-md hover:bg-gray-100 transition">
             Discard
           </button>
 
-          <button className="px-5 py-2 bg-red-600 text-white rounded-md hover:bg-red-700 transition">
+          <button className="px-7 py-4 text-2xl bg-red-600 text-white rounded-md hover:bg-red-700 transition">
             Post
           </button>
         </div>
